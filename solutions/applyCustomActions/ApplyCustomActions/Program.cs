8273ad7ade0cec5835f68e9f83a0eb5cbae4a337
Phillip Demro <phillip.demro@hotmail.com>
2016-03-07 11:40:14 -0500

    added solution for applying custom action to sites

---
+++ solutions/applyCustomActions/ApplyCustomActions/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.SharePoint.Client;
+using System.Security;
+using System.Threading;
+
+namespace ApplyCustomActions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor defaultForeground = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Enter the URL of the SharePoint Online site:");
+
+            Console.ForegroundColor = defaultForeground;
+            //string webUrl = Console.ReadLine();
+            string webUrl = "https://mod821521.sharepoint.com/sites/demroTeam/test2";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Enter your user name (ex: kirke@mytenant.microsoftonline.com):");
+            Console.ForegroundColor = defaultForeground;
+            //string userName = Console.ReadLine();
+            string userName = "pdemro@mod821521.onmicrosoft.com";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Enter your password.");
+            Console.ForegroundColor = defaultForeground;
+
+            
+
+            //var password = new SecureString();
+            var password = GetPasswordFromConsoleInput();
+            //foreach (var c in passwordArray)
+            //{
+            //    password.AppendChar(c);
+            //}
+
+            
+
+
+            using (var context = new ClientContext(webUrl))
+            {
+                context.Credentials = new SharePointOnlineCredentials(userName, password);
+                context.Load(context.Web, w => w.Title);
+                context.ExecuteQuery();
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Your site title is: " + context.Web.Title);
+                Console.WriteLine("Setting custom action..");
+                SetCustomAction(context.Web);
+
+
+                Console.ForegroundColor = defaultForeground;
+                Console.ReadLine();
+            }
+
+        }
+
+        private static void SetCustomAction(Web web)
+        {
+            var targetAction = web.UserCustomActions.Add();
+
+            targetAction.Name = "Test Custom Action";
+            targetAction.Description = "Test Custom Action Description";
+            targetAction.Location = "ScriptLink";
+            targetAction.Sequence = 100;
+            targetAction.ScriptSrc = "~SiteCollection/_catalogs/_app/test/test.js";
+
+            targetAction.Update();
+
+            web.Context.Load(web, w => w.UserCustomActions);
+            web.Context.ExecuteQueryRetry();
+        }
+
+        private static SecureString GetPasswordFromConsoleInput()
+        {
+            ConsoleKeyInfo info;
+
+            //Get the user's password as a SecureString
+            SecureString securePassword = new SecureString();
+            do
+            {
+                info = Console.ReadKey(true);
+                if (info.Key != ConsoleKey.Enter)
+                {
+                    securePassword.AppendChar(info.KeyChar);
+                }
+            }
+            while (info.Key != ConsoleKey.Enter);
+            return securePassword;
+        }
+    }
+}
---
+++ solutions/applyCustomActions/ApplyCustomActions/extensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace ApplyCustomActions
+{
+    public static class extensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="clientContext"></param>
+        /// <param name="retryCount">Number of times to retry the request</param>
+        /// <param name="delay">Milliseconds to wait before retrying the request. The delay will be increased (doubled) every retry</param>
+        public static void ExecuteQueryRetry(this ClientRuntimeContext clientContext, int retryCount = 10, int delay = 500)
+        {
+            ExecuteQueryImplementation(clientContext, retryCount, delay);
+        }
+
+        private static void ExecuteQueryImplementation(ClientRuntimeContext clientContext, int retryCount = 10, int delay = 500)
+        {
+            int retryAttempts = 0;
+            int backoffInterval = delay;
+            if (retryCount <= 0)
+                throw new ArgumentException("Provide a retry count greater than zero.");
+
+            if (delay <= 0)
+                throw new ArgumentException("Provide a delay greater than zero.");
+
+            // Do while retry attempt is less than retry count
+            while (retryAttempts < retryCount)
+            {
+                try
+                {
+                    clientContext.ExecuteQuery();
+                    return;
+
+                }
+                catch (WebException wex)
+                {
+                    var response = wex.Response as HttpWebResponse;
+                    // Check if request was throttled - http status code 429
+                    // Check is request failed due to server unavailable - http status code 503
+                    if (response != null && (response.StatusCode == (HttpStatusCode)429 || response.StatusCode == (HttpStatusCode)503))
+                    {
+                        Debug.WriteLine("CSOM request frequency exceeded usage limits. Sleeping for {0} seconds before retrying.", backoffInterval);
+
+                        //Add delay for retry
+                        Thread.Sleep(backoffInterval);
+
+                        //Add to retry count and increase delay.
+                        retryAttempts++;
+                        backoffInterval = backoffInterval * 2;
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            throw new Exception($"Maximum retry attempts {retryCount}, has be attempted.");
+        }
+    }
+}
