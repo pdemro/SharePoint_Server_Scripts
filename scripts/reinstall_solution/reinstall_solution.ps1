869368759825a27cd575281ab0820deff38f7b66
Phillip Demro <phillip.demro@hotmail.com>
2017-01-11 09:20:28 -0500

    updated WSP deployment script

--- scripts/reinstall_solution/reinstall_solution.ps1
+++ scripts/reinstall_solution/reinstall_solution.ps1
@@ -1,44 +1,109 @@
 ﻿add-pssnapin microsoft.sharepoint.powershell
 
-$webUrl = "http://sharepoint"
-$caUrl = "https://sharepointCa:12345"
-$solutionName = "customsolution.wsp"
-$solutionDir = "c:\users\a-pdemro\documents\"
-$solutionFullName = $solutionDir + $solutionName
-$dependenciesPath = "C:\users\a-pdemro\source\repos\SharePoint_Server_Scripts\scripts\reinstall_solution\dependencies"
+function reinstallSolution {
+    Param(
+      [string]$codeDirectory,
+      [string]$solutionTitle,
+      [string]$directory,
+      [array]$warmupWebs
+    )
+    $solutionName = $solutionTitle+".wsp"
 
-$farm = get-spfarm
-$solution = $farm.Solutions | where-object {$_.Name -eq $solutionName}
+    $solutionFullName = $directory+$solutionName
+    $farm = get-spfarm
+    $solution = $farm.Solutions | where-object {$_.Name -eq $solutionName}
 
-uninstall-spsolution -identity $solutionName -webapplication $webUrl -Confirm:$false
+    uninstall-spsolution -identity $solutionName -Confirm:$false
 
-while($solution.Deployed -eq $true){
-    write-host "waiting to uninstall.."
+    while($solution.Deployed -eq $true){
+        write-host "waiting to uninstall.."
+        start-sleep -seconds 5
+    }
+
+    while($solution.JobExists -eq $true) {
+        write-host "waiting for the solution to actually be retracted"
+        start-sleep -seconds 5
+    }
+
+    remove-spsolution -identity $solutionName -Confirm:$false
+
+    #write-host "wait"
+    #start-sleep -seconds 2000
+
+    add-spsolution -literalpath $solutionFullName
+
+    write-host "Added solution.. giving it a few seconds"
     start-sleep -seconds 5
+
+    install-spsolution -identity $solutionName -gacdeployment -Force
+
+    $farm = get-spfarm
+    $customKeywordSearchSol = $farm.Solutions | where-object {$_.Name -eq $solutionName}
+
+    start-sleep -seconds 10
+
+    while($customKeywordSearchSol.Deployed -eq $false) {
+        write-host "waiting for the solution to be deployed"
+        start-sleep -seconds 5
+    }
+
+    write-host "Solution deployment for" $solutionName "Complete"
 }
 
-while($solution.JobExists -eq $true) {
-    write-host "waiting for the solution to actually be retracted"
-    start-sleep -seconds 5
+#not used
+function upgradeSolution {
+    Param(
+      [string]$codeDirectory,
+      [string]$solutionTitle,
+      [string]$directory,
+      [array]$warmupWebs
+    )
+    $solutionName = $solutionTitle+".wsp"
+
+    $solutionFullName = $directory+$solutionName
+    
+    update-spsolution -identity $solutionName -literalpath $solutionFullName -gacdeployment
+
+    $farm = get-spfarm
+    $solution = $farm.Solutions | where-object {$_.Name -eq $solutionName}
+
+    write-host "Sent update command.. waiting a few seconds"
+    start-sleep -seconds 15
+
+    while($solution.Status -ne "Online" -or $customKeywordSearchSol.Deployed -eq $false) {
+        write-host "waiting for the solution to be deployed"
+        start-sleep -seconds 5
+    }
+
+    write-host "Solution update for" $solutionName "Complete"
 }
 
-#start-sleep -seconds 20
+#Make sure to build/save packages in visual studio prior to executing this script
+msbuild /t:Package C:\path\to\project\project.csproj
+mv C:\path\to\project\bin\Debug\project.wsp c:\path\to\packages\project.wsp -Force
 
-remove-spsolution -identity $solutionName -Confirm:$false
+msbuild /t:Package c:\path\to\project2\project2.csproj
+mv c:\path\to\project2\bin\Debug\project2.wsp c:\path\to\packages\project2.wsp -Force
 
-add-spsolution -literalpath $solutionFullName
+#reinstallSolution -solutionTitle "project.wsp" -directory "c:\path\to\packages\" -warmupWebs $warmupWebs
+reinstallSolution -solutionTitle "project2.wsp" -directory "c:\path\to\packages\" -warmupWebs $warmupWebs
 
-write-host "Added solution.. giving it 10 seconds or so"
-start-sleep -seconds 10
+$siteUrl = "<siteUrl>"
 
-install-spsolution -identity $solutionName -gacdeployment -webapplication $webUrl
+$site = get-spsite($siteUrl)
+$site.Delete()
 
-$farm = get-spfarm
-$solution = $farm.Solutions | where-object {$_.Name -eq $solutionName}
+#Can't remember why this was necessary - pretty sure permissions related
+$command = "add-pssnapin microsoft.sharepoint.powershell;new-spsite -name 'Test Collaboration Site' -url $siteUrl -OwnerAlias 'sp16\pdemro' -Language 1033 -Template 'MTCOLLAB#0'"
+powershell.exe -command $command
+#new-spsite -url $siteUrl -OwnerAlias "sp16\pdemro" -Language 1033 -Template "MTCOLLAB#0"
 
-while($solution.Status -ne "Online") {
-    write-host "waiting for the solution to be deployed"
-    start-sleep -seconds 5
-}
+#iisreset
 
-C:\users\a-pdemro\source\repos\SharePoint_Server_Scripts\scripts\reinstall_solution\dependencies\spbestwarmup.ps1 -url $webUrl,$caUrl
+$warmupWebs = $siteUrl,"http://sp16dev:12015","http://my.sp16dev.com","http://sp16dev"
+C:\path\to\repos\SharePoint_server_scripts\reinstall_solution\dependencies\spbestwarmup.ps1 -url $warmupWebs
+
+
+#Enable-SPFeature -Identity "e44eda44-7398-4a51-b4fc-dab9f7b75c6b" -Url $siteUrl
+
+
