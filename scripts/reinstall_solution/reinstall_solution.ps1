756dacabc05d7188e2458eafe67dc3365c8fe3f0
Phillip Demro <phillip.demro@hotmail.com>
2018-08-20 09:17:26 -0400

    tweaks for simple deployment

--- scripts/reinstall_solution/reinstall_solution.ps1
+++ scripts/reinstall_solution/reinstall_solution.ps1
@@ -79,29 +79,34 @@ function upgradeSolution {
 }
 
 #Make sure to build/save packages in visual studio prior to executing this script
-msbuild /t:Package C:\path\to\project\project.csproj
-mv C:\path\to\project\bin\Debug\project.wsp c:\path\to\packages\project.wsp -Force
+msbuild /t:Package C:\wsb\cvng\Mantech-ConstantView-Upgrade\src\Mantech.ConstantView.SharePoint.Solution\Mantech.ConstantView.SharePoint.Solution.csproj
+mv C:\wsb\cvng\Mantech-ConstantView-Upgrade\src\Mantech.ConstantView.SharePoint.Solution\bin\Debug\Mantech.ConstantView.SharePoint.wsp C:\wsb\cvng\Mantech-ConstantView-Upgrade\Mantech.ConstantView.SharePoint.wsp -Force
 
-msbuild /t:Package c:\path\to\project2\project2.csproj
-mv c:\path\to\project2\bin\Debug\project2.wsp c:\path\to\packages\project2.wsp -Force
+# msbuild /t:Package c:\path\to\project2\project2.csproj
+# mv c:\path\to\project2\bin\Debug\project2.wsp c:\path\to\packages\project2.wsp -Force
 
-#reinstallSolution -solutionTitle "project.wsp" -directory "c:\path\to\packages\" -warmupWebs $warmupWebs
-reinstallSolution -solutionTitle "project2.wsp" -directory "c:\path\to\packages\" -warmupWebs $warmupWebs
+#reinstallSolution -solutionTitle "Mantech.ConstantView.App" -directory "C:\Users\pdemro\Desktop\deployment\2018-08-15_11-03\" -warmupWebs $warmupWebs
+reinstallSolution -solutionTitle "Mantech.ConstantView.SharePoint" -directory "C:\wsb\cvng\Mantech-ConstantView-Upgrade\" -warmupWebs $warmupWebs
 
-$siteUrl = "<siteUrl>"
+#$siteUrl = "http://demro.sp16.com"
 
-$site = get-spsite($siteUrl)
-$site.Delete()
+# Testing a site definition (delete and recreate every time)
+# $site = get-spsite($siteUrl)
+# $site.Delete()
 
 #Can't remember why this was necessary - pretty sure permissions related
-$command = "add-pssnapin microsoft.sharepoint.powershell;new-spsite -name 'Test Collaboration Site' -url $siteUrl -OwnerAlias 'sp16\pdemro' -Language 1033 -Template 'MTCOLLAB#0'"
-powershell.exe -command $command
+# $command = "add-pssnapin microsoft.sharepoint.powershell;new-spsite -name 'Test Collaboration Site' -url $siteUrl -OwnerAlias 'sp16\pdemro' -Language 1033 -Template 'MTCOLLAB#0'"
+# powershell.exe -command $command
 #new-spsite -url $siteUrl -OwnerAlias "sp16\pdemro" -Language 1033 -Template "MTCOLLAB#0"
 
-#iisreset
+iisreset
 
-$warmupWebs = $siteUrl,"http://sp16dev:12015","http://my.sp16dev.com","http://sp16dev"
-C:\path\to\repos\SharePoint_server_scripts\reinstall_solution\dependencies\spbestwarmup.ps1 -url $warmupWebs
+net stop SPTimerV4
+net start SPTimerV4
+
+# $warmupWebs = "http://win-bon5g1qr9mv:2016/", $siteUrl, "http://cv.sp16.com/sites/DemroXI"
+# C:\Users\pdemro\source\repos\SharePoint_Server_Scripts\scripts\reinstall_solution\dependencies\spbestwarmup.ps1 -url $warmupWebs
+spbestwarmup.ps1 #assumes spbestwarmup is in the path
 
 
 #Enable-SPFeature -Identity "e44eda44-7398-4a51-b4fc-dab9f7b75c6b" -Url $siteUrl
